24c9b434da5ab543ecfd9a0b616c4df9e5d2361a
Atif8ted <mohd.atifimam@hotmail.com>
2018-04-05 12:36:52 +0530

    update

---
+++ src/day1/Arrayobj.java
@@ -0,0 +1,24 @@
+package day1;
+import java.util.*;
+
+public class Arrayobj{
+	public static void main(String args[]){
+		Addition ob[]=new Addition[3];
+		ob[0]=new Addition();
+		ob[1]=new Addition();
+		ob[2]=new Addition();
+		Scanner sc=new Scanner(System.in);
+		ob[0].setdata(sc.nextInt(),sc.nextInt());
+		ob[0].display();
+		ob[0].add();
+		ob[0].display();
+		ob[1].setdata(sc.nextInt(),sc.nextInt());
+		ob[1].display();
+		ob[1].add();
+		ob[1].display();
+		ob[2].setdata(sc.nextInt(),sc.nextInt());
+		ob[2].display();
+		ob[2].add();
+		ob[2].display();
+	}
+}
---
+++ src/day1/MainforAdditionforArray.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class MainforAdditionforArray {
+
+	public static void main(String[] args) {
+		AdditionOfArray ob=new AdditionOfArray();
+		// TODO Auto-generated method stub
+
+	}
+
+}

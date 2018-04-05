1d553d63b19aa72cead9c48db08c3896c7186e78
Atif8ted <mohd.atifimam@hotmail.com>
2018-04-05 12:16:27 +0530

    done

--- src/day1/MainForAddition.java
+++ src/day1/MainForAddition.java
@@ -1,11 +1,15 @@
 package day1;
 
+import java.util.Scanner;
+
 public class MainForAddition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Addition ob=new Addition();
-		ob.setdata(50, 60);
+		System.out.println("Enter the two value");
+		Scanner sc=new Scanner(System.in);
+		ob.setdata(sc.nextInt(),sc.nextInt());
 		ob.display();
 		ob.add();
 		ob.display();
